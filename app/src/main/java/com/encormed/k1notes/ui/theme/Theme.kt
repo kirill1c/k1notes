702b3dd030d581ba233bc16db088ecbf9e75cf40
kirill1c <kirill1c@mail.ru>
2024-04-10 20:46:10 +0300

    add theme

--- app/src/main/java/com/encormed/k1notes/ui/theme/Theme.kt
+++ app/src/main/java/com/encormed/k1notes/ui/theme/Theme.kt
@@ -10,15 +10,18 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.White,
+    secondary = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -65,6 +68,7 @@ fun K1notesTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
